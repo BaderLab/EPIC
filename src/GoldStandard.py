143414289e65c24c52103d79b40574ea0fc9316a
LucasHu <ming.sebastian.unsw@gmail.com>
2018-05-12 14:24:52 -0400

    add GOurl print

--- src/GoldStandard.py
+++ src/GoldStandard.py
@@ -760,6 +760,7 @@ class QuickGO():
 		prot_to_go_map = {}
 		quickgoURL = "http://www.ebi.ac.uk/QuickGO-Old/GAnnotation?goid=GO:0043234&tax=%s&format=tsv&limit=1000000000&evidence=IDA,IPI,EXP," % (self.taxid)
 		print quickgoURL
+		print self.taxid
 		print "the url is: ..."
 		quickgoURL_FH = urllib2.urlopen(quickgoURL)
 		quickgoURL_FH.readline()
